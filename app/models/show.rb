adf0afbd9b3bb769ca4d86e438fc307002dec11c
Ryan Seit <ryan@ryanseit.com>
2020-03-01 18:17:32 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
-  
+
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.map {|show| show.actor.first_name + " " + show.actor.last_name}
+    end
+
+    def build_network(call_letters)
+        
+    end
+
 end
+
+# binding.pry
