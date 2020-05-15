d7005d9ffce375506dace474c155626b554b6e59
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-05-14 22:25:08 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,24 @@
+require "pry"
+
 class Show < ActiveRecord::Base
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
   
-end
+  def actors_list
+    actors.map do |name|
+      "#{name.first_name} #{name.last_name}"  
+    
+    end 
+
+  end
+
+end 
+
+
+
+# def list_roles
+#   characters.map do |character|
+#       "#{character.name} - #{character.show.name}"
+#   end 
+#   end 
